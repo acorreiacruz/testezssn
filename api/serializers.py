0c0c101d7d13fb86b06f43b06619a2c2b42de1de
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-04-20 17:05:26 -0300

    Alterando os serializers

--- api/serializers.py
+++ api/serializers.py
@@ -3,12 +3,17 @@ from rest_framework import serializers
 from .models import Inventario, Sobrevivente
 
 
-class SobreviventeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sobrevivente
-        fields = ('id','nome','idade','sexo','infectado')
-
 class InventarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Inventario
         fields = ('id','agua','alimentacao','medicacao','municao','sobrevivente')
+
+class SobreviventeSerializer(serializers.ModelSerializer):
+
+    inventario = InventarioSerializer()
+
+    class Meta:
+        model = Sobrevivente
+        fields = ('id','nome','idade','sexo','infectado','inventario')
+
+
