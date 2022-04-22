f2241a903ce989b9738c2340677cf793ae0f98d0
Antonio Correia <antoniocorreiacruz@outlook.com>
2022-04-21 23:15:12 -0300

    Criando a classe serializer para o model Local.

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 
-from .models import Inventario, Sobrevivente
+from .models import Inventario, Sobrevivente, Local
 
+
+class LocalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Local
+        fields = ('latitude','longitude','sobrevivente')
 
 class InventarioSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,9 +16,10 @@ class InventarioSerializer(serializers.ModelSerializer):
 class SobreviventeSerializer(serializers.ModelSerializer):
 
     inventario = InventarioSerializer(many=False, read_only=True)
+    local = LocalSerializer(many=False, read_only=True)
 
     class Meta:
         model = Sobrevivente
-        fields = ('id','nome','idade','sexo','infectado','inventario','quant_denuncias')
+        fields = ('id','nome','idade','sexo','infectado','inventario','quant_denuncias','local')
 
 
