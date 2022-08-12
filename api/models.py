ff7124d6563585e2fc82efb363f612c5889d1fc9
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-12 18:47:25 -0300

    Alterando os models do projeto e colocando todos os dados em um mesmo model

--- api/models.py
+++ api/models.py
@@ -15,20 +15,13 @@ class Sobrevivente(models.Model):
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
     denuncias = models.PositiveIntegerField(null=False, blank=False, default=0)
-
-    def __str__(self) -> str:
-        return self.nome
-
-class Local(models.Model):
-
     latitude = models.DecimalField(max_digits=22, decimal_places=16)
     longitude = models.DecimalField(max_digits=22, decimal_places=16)
-    sobrevivente = models.OneToOneField(Sobrevivente,on_delete=models.CASCADE, null=False)
-
-class Inventario(models.Model):
-
     agua = models.PositiveIntegerField(null=False, default=0)
     alimentacao = models.PositiveIntegerField(null=False, default=0)
     medicacao = models.PositiveIntegerField(null=False, default=0)
     municao = models.PositiveIntegerField(null=False, default=0)
-    sobrevivente = models.OneToOneField(Sobrevivente, on_delete=models.CASCADE, null=False)
+
+    def __str__(self) -> str:
+        return self.nome
+
