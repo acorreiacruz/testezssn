ba2156ad5b146afb9fbc7b334d878e06067963a5
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-12 20:30:16 -0300

    Alterando os valores default do model Sobrevivente.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 from django.db import models
 
 
@@ -11,16 +12,16 @@ class Sobrevivente(models.Model):
     nome = models.CharField(null=False, blank=True, max_length=100)
     idade = models.PositiveIntegerField(null=False, blank=True,)
     sexo = models.CharField(choices= SEXO_CHOICES, null=False, blank=False,max_length=1)
-    infectado = models.BooleanField()
+    infectado = models.BooleanField(default=False)
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
     denuncias = models.PositiveIntegerField(null=False, blank=False, default=0)
-    latitude = models.DecimalField(max_digits=22, decimal_places=16)
-    longitude = models.DecimalField(max_digits=22, decimal_places=16)
-    agua = models.PositiveIntegerField(null=False, default=0)
-    alimentacao = models.PositiveIntegerField(null=False, default=0)
-    medicacao = models.PositiveIntegerField(null=False, default=0)
-    municao = models.PositiveIntegerField(null=False, default=0)
+    latitude = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+    longitude = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+    agua = models.PositiveIntegerField(null=False)
+    alimentacao = models.PositiveIntegerField(null=False)
+    medicacao = models.PositiveIntegerField(null=False)
+    municao = models.PositiveIntegerField(null=False)
 
     def __str__(self) -> str:
         return self.nome
