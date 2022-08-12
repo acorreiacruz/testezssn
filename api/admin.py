c1a9225ac22943eaff9e4e97a84bf30c8ed37821
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-12 18:51:30 -0300

    Removendo os models excluídos e deixando apeanas o model Sobrevivente no admin.

--- api/admin.py
+++ api/admin.py
@@ -5,12 +5,3 @@ from .models import Inventario, Sobrevivente, Local
 @admin.register(Sobrevivente)
 class SobreviventeAdmin(admin.ModelAdmin):
     list_display = ('id','nome','idade','sexo','infectado','denuncias')
-
-
-@admin.register(Inventario)
-class InventarioAdmin(admin.ModelAdmin):
-    list_display = ('sobrevivente','agua','alimentacao','medicacao','municao')
-
-@admin.register(Local)
-class LocalAdmin(admin.ModelAdmin):
-    list_display = ('latitude','longitude','sobrevivente')
