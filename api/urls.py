44230ecf293e820a445585eb874e1d63c34ab97a
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-12 18:52:15 -0300

    Excluindo as urls obsoletas após exclusão dos models.

--- api/urls.py
+++ api/urls.py
@@ -5,39 +5,29 @@ app_name = "api"
 
 urlpatterns = [
     path(
-        'sobreviventes/denuncias/<int:pk>/',
+        'sobreviventes/<int:pk>/denuncia/',
         views.denunciar_infectado,
         name='denunciar_infectado'
     ),
     path(
         'sobreviventes/<int:pk>/',
-        views.SobreviventeRetriveUpdateDestroyAPIView.as_view(),
-        name="detalhar_alterar_deletar_sobrevivente"
+        views.SobreviventeModelViewSet.as_view(
+            {
+                'get': 'retrieve',
+                'patch': 'partial_update',
+                'delete': 'destroy'
+            }
+        ),
+        name='sobrevivente_buscar_alterar_excluir'
     ),
     path(
         'sobreviventes/',
-        views.SobreviventeListCreateAPIView.as_view(),
-        name="listar_postar_sobrevivente"
+        views.SobreviventeModelViewSet.as_view(
+            {
+                'get': 'list',
+                'post': 'create'
+            }
+        ),
+        name="sobrevivente_listar_criar"
     ),
-    path(
-        'trocas/<int:id1>/<str:item1>/<int:qnt1>/<int:id2>/<str:item2>/<int:qnt2>/',
-        views.InvetarioNegociar.as_view(),
-        name="negociar_itens"
-
-    ),
-    path(
-        'inventarios/<int:pk>/',
-        views.InventarioRetriveUpdateDestroyAPIView.as_view(),
-        name="detalhar_alterar_deletar_inventario"
-    ),
-    path(
-        'inventarios/',
-        views.InventarioListCreateAPIView.as_view(),
-        name="inventarios"
-    ),
-    path(
-        'sobreviventes/local/<int:id>/',
-        views.LocalAPIView.as_view(),
-        name='detalhar_atualizar_local'
-    )
 ]
