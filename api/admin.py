fce2a31a1c892406cfca2a0fd2eab50ac40be7a2
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-12 18:53:15 -0300

    Atualizando os campos exibidos na pagina de admin.

--- api/admin.py
+++ api/admin.py
@@ -4,4 +4,8 @@ from .models import Inventario, Sobrevivente, Local
 
 @admin.register(Sobrevivente)
 class SobreviventeAdmin(admin.ModelAdmin):
-    list_display = ('id','nome','idade','sexo','infectado','denuncias')
+    list_display = (
+        'id','nome','idade','sexo','infectado',
+        'denuncias','latitude', 'longitudade',
+        'agua','alimentacao','medicacao','municao'
+    )
