b20521fb228f4d22fe675a94391db5364a46d0fc
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-12 21:33:05 -0300

    Exlcuindo o arquivo permissions.py

--- api/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class PermissoesInfectado(BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        return True if obj.infectado else False
-
-    def has_permission(self, request, view):
-        return super().has_permission(request, view)
