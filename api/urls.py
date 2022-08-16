3cd351288bbde7080dc62cfc5b0df4f589a13cd6
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-16 20:09:30 -0300

    Removendo as urls hard coded e utilizando um simple router.

--- api/urls.py
+++ api/urls.py
@@ -1,34 +1,24 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import SimpleRouter
+
 
 app_name = "api"
+
+
+api_sobreviventes_urls = SimpleRouter()
+api_sobreviventes_urls.register(
+    'sobreviventes',
+    views.SobreviventeModelViewSet,
+    basename='api-sobreviventes'
+)
+
 
 urlpatterns = [
     path(
         'sobreviventes/<int:pk>/denuncia/',
         views.denunciar_infectado,
         name='denunciar_infectado'
-    ),
-    path(
-        'sobreviventes/<int:pk>/',
-        views.SobreviventeModelViewSet.as_view(
-            {
-                'get': 'retrieve',
-                'patch': 'partial_update',
-                'delete': 'destroy'
-            }
-        ),
-        name='buscar_alterar_excluir_sobrevivente'
-    ),
-    path(
-        'sobreviventes/',
-        views.SobreviventeModelViewSet.as_view(
-            {
-                'get': 'list',
-                'post': 'create'
-            }
-        ),
-        name="listar_criar_sobrevivente"
     ),
     path(
         'sobreviventes/trocas/<int:id1>/<str:itm1>/<int:qnt1>/<int:id2>/<str:itm2>/<int:qnt2>/',
@@ -51,3 +41,5 @@ urlpatterns = [
         name='medias_do_inventario'
     ),
 ]
+
+urlpatterns += api_sobreviventes_urls.urls
