51118db7061e77422ff39d01c7ada19a223b228d
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-16 21:20:12 -0300

    Definindo a configuração de renderer classes para a API

--- project/settings.py
+++ project/settings.py
@@ -118,5 +118,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Configurações do Django Rest Framework
 
 REST_FRAMEWORK = {
-
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
 }
