74fb488ac4fd92fb8e66498a994acc826530438e
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-17 23:20:55 -0300

    Consertando o erro da queryset do RelatorioViewSet.

--- api/views/relatorio.py
+++ api/views/relatorio.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from ..serializers import SobreviventeSerializer
 from rest_framework.response import Response
 from ..models import Sobrevivente
 from rest_framework import viewsets
@@ -8,7 +8,8 @@ from django.db.models import Avg
 
 class RelatorioViewSet(viewsets.GenericViewSet):
 
-    serializer_class = Sobrevivente.objects.all()
+    queryset = Sobrevivente.objects.all()
+    serializer_class = SobreviventeSerializer
     http_method_names = ['get']
 
     def get_queryset_qnt_objects(self):
