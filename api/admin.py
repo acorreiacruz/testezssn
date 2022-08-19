c8c5452a228fa055691f660b561a4d2096254d99
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-19 16:09:21 -0300

    Adicionando os ModelAdmin para o inventário e o local

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Sobrevivente
+from .models import Inventario, Sobrevivente, Local
 
 
 @admin.register(Sobrevivente)
@@ -8,5 +8,20 @@ class SobreviventeAdmin(admin.ModelAdmin):
         'id', 'nome', 'idade', 'sexo', 'infectado',
         'denuncias', 'ultimo_local', 'inventario'
     )
+
+
+@admin.register(Inventario)
+class InventarioAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'agua', 'alimentacao',
+        'medicacao', 'municao'
+    ]
+
+
+@admin.register(Local)
+class LocalAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'latitude', 'longitude',
+    ]
 
 
