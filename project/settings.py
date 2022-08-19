b5d85fb2245e067890917d1e06ed47b9d9be4579
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-19 09:45:39 -0300

    Adicionando um novo BD ao projeto.

--- project/settings.py
+++ project/settings.py
@@ -70,6 +70,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'new':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testezssn',
+        'USER': 'root',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
