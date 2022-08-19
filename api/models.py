6f60918a09b4ca8be69d32cdde7e71b1cd1c1ae4
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-19 10:53:02 -0300

    Consertando problemas no BD em relação ao campo denúncias.

--- api/models.py
+++ api/models.py
@@ -1,4 +1,3 @@
-from pickle import FALSE
 from django.db import models
 
 
@@ -9,19 +8,19 @@ class Sobrevivente(models.Model):
         ('F','Feminino')
     )
 
-    nome = models.CharField(null=False, blank=True, max_length=100)
-    idade = models.PositiveIntegerField(null=False, blank=True,)
+    nome = models.CharField(null=False, blank=False, max_length=100)
+    idade = models.PositiveIntegerField(null=False, blank=False)
     sexo = models.CharField(choices= SEXO_CHOICES, null=False, blank=False,max_length=1)
-    infectado = models.BooleanField(default=False)
+    infectado = models.BooleanField(null=False, blank=False, default=False)
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
     denuncias = models.PositiveIntegerField(null=False, blank=False, default=0)
     latitude = models.DecimalField(max_digits=22, decimal_places=16, null=True)
     longitude = models.DecimalField(max_digits=22, decimal_places=16, null=True)
-    agua = models.PositiveIntegerField(null=False)
-    alimentacao = models.PositiveIntegerField(null=False)
-    medicacao = models.PositiveIntegerField(null=False)
-    municao = models.PositiveIntegerField(null=False)
+    agua = models.PositiveIntegerField(null=False, default=0)
+    alimentacao = models.PositiveIntegerField(null=False, default=0)
+    medicacao = models.PositiveIntegerField(null=False, default=0)
+    municao = models.PositiveIntegerField(null=False, default=0)
 
     def __str__(self) -> str:
         return self.nome
