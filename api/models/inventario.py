7e5e6da0a9b907abf962e955f9c74f2f4f2bf940
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-19 11:12:32 -0300

    Apagando os outros arquivos de models.

--- api/models/inventario.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Inventario(models.Model):
-    agua = models.PositiveIntegerField(null=False, blank=False, default=0)
-    alimentacao = models.PositiveIntegerField(null=False, blank=False, default=0)
-    medicacao = models.PositiveIntegerField(null=False, blank=False, default=0)
-    municao = models.PositiveIntegerField(null=False, blank=False, default=0)
--- api/models/local.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Local(models.Model):
-    latitude = models.DecimalField(max_digits=22, decimal_places=16, null=True)
-    longitude = models.DecimalField(max_digits=22, decimal_places=16, null=True)
