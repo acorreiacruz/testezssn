7cad8a6b14cc0471bf0f330e0016fb49406127de
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-19 15:38:22 -0300

    Criando os models separados para o inventário e para o local.

--- api/models.py
+++ api/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
 
+class Inventario(models.Model):
+    agua = models.PositiveIntegerField(null=False, default=0)
+    alimentacao = models.PositiveIntegerField(null=False, default=0)
+    medicacao = models.PositiveIntegerField(null=False, default=0)
+    municao = models.PositiveIntegerField(null=False, default=0)
+
+
+class Local(models.Model):
+    latitude = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+    longitude = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+
+
 class Sobrevivente(models.Model):
 
-    SEXO_CHOICES = (
-        ('M','Masculino'),
-        ('F','Feminino')
-    )
+    SEXO_CHOICES = (("M", "Masculino"), ("F", "Feminino"))
 
     nome = models.CharField(null=False, blank=False, max_length=100)
     idade = models.PositiveIntegerField(null=False, blank=False)
-    sexo = models.CharField(choices= SEXO_CHOICES, null=False, blank=False,max_length=1)
+    sexo = models.CharField(choices=SEXO_CHOICES, null=False, blank=False, max_length=1)
     infectado = models.BooleanField(null=False, blank=False, default=False)
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
@@ -21,7 +30,6 @@ class Sobrevivente(models.Model):
     alimentacao = models.PositiveIntegerField(null=False, default=0)
     medicacao = models.PositiveIntegerField(null=False, default=0)
     municao = models.PositiveIntegerField(null=False, default=0)
-
 
     def __str__(self) -> str:
         return self.nome
