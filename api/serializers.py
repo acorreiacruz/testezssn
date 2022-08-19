569964982b442e984a4db09960dd64f0f50b3d0a
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-19 15:45:11 -0300

    Criando os serializers do inventário e do local

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,19 @@
+from dataclasses import fields
 from rest_framework import serializers
-from .models import Sobrevivente
+from .models import Sobrevivente, Inventario, Local
+
+
+class LocalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Local
+        fields = ['id', 'latitude', 'longitude']
+
+
+class InventarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Inventario
+        fields = ['id', 'agua', 'alimentacao', 'medicacao', 'municao']
+
 
 
 class SobreviventeSerializer(serializers.ModelSerializer):
