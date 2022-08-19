397f4f0095cbbd9a1e1e0083330262e2b6ca1e01
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-08-19 15:34:16 -0300

    Alterando a formatação do código do model.

--- api/models.py
+++ api/models.py
@@ -3,14 +3,11 @@ from django.db import models
 
 class Sobrevivente(models.Model):
 
-    SEXO_CHOICES = (
-        ('M','Masculino'),
-        ('F','Feminino')
-    )
+    SEXO_CHOICES = (("M", "Masculino"), ("F", "Feminino"))
 
     nome = models.CharField(null=False, blank=False, max_length=100)
     idade = models.PositiveIntegerField(null=False, blank=False)
-    sexo = models.CharField(choices= SEXO_CHOICES, null=False, blank=False,max_length=1)
+    sexo = models.CharField(choices=SEXO_CHOICES, null=False, blank=False, max_length=1)
     infectado = models.BooleanField(null=False, blank=False, default=False)
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
@@ -21,7 +18,6 @@ class Sobrevivente(models.Model):
     alimentacao = models.PositiveIntegerField(null=False, default=0)
     medicacao = models.PositiveIntegerField(null=False, default=0)
     municao = models.PositiveIntegerField(null=False, default=0)
-
 
     def __str__(self) -> str:
         return self.nome
