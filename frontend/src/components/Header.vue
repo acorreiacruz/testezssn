7e14ed2ae8443650ae43582a88e029b3c69c917f
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-18 22:06:41 -0300

    Alterando o CSS a aba atual da NavBar.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,16 +3,16 @@
         <h1 class="header-title">ZSSN</h1>
         <div class="menu-container">
             <div class="link-container">
-                <router-link to="/">Sobreviventes</router-link>
+                <router-link id="sobreviventes" to="/" @click="botaoAtual($event)">Sobreviventes</router-link>
             </div>
             <div class="link-container">
-                <router-link to="/inventarios">Inventários</router-link>
+                <router-link id="inventarios" to="/inventarios" @click="botaoAtual($event)">Inventários</router-link>
             </div>
             <div class="link-container">
-                <router-link to="/comercio">Comércio</router-link>
+                <router-link id="comercio" to="/comercio" @click="botaoAtual($event)">Comércio</router-link>
             </div>
             <div class="link-container">
-                <router-link to="/relatorios">Relatórios</router-link>
+                <router-link id="relatorios" to="/relatorios" @click="botaoAtual($event)">Relatórios</router-link>
             </div>
         </div>
     </header>
@@ -23,11 +23,22 @@ export default {
     name:"NavBar",
     data(){
         return {
-            link_sobreviventes: "#",
-            link_inventarios: "#",
-            link_comercio: "#",
-            link_relatorios: "#"
+            flag: true,
+            anterior: null,
         }
-    }
+    },
+    methods:{
+        botaoAtual(e){
+            const el = e.target;
+            if(!this.anterior){
+                el.style.backgroundColor = "rgb(36, 36, 36)";
+                this.anterior = el.id;
+            }
+            const anterior = document.getElementById(this.anterior);
+            anterior.style.backgroundColor = "rgb(0, 0, 0)";
+            el.style.backgroundColor = "rgb(36, 36, 36)";
+            this.anterior = el.id;
+        }
+    },
 }
 </script>
